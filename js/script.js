763bc393227c00e49571990fc399efb6b243b12a
Laura Pais <pais.laura@gmail.com>
2024-10-02 17:17:11 +0100

    ++

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ window.onload = function() {
     let directionsRight = Array.from(moveRightElements).map(() => true);
     let directionsLeft = Array.from(moveLeftElements).map(() => true);
 
+    // Adiciona a margem de 10% além da largura da janela
+    const margin = window.innerWidth * 0.4;
+
     setTimeout(() => {
         moveRightElements.forEach(el => el.style.opacity = '1');
         moveLeftElements.forEach(el => el.style.opacity = '1');
@@ -15,12 +18,12 @@ window.onload = function() {
             const currentPos = parseFloat(getComputedStyle(el).left);
             if (directionsRight[index]) {
                 el.style.left = (currentPos + 0.5) + 'px';
-                if (currentPos > window.innerWidth) {
+                if (currentPos > window.innerWidth + margin) { // Verifica com a margem adicional
                     directionsRight[index] = false; 
                 }
             } else {
                 el.style.left = (currentPos - 0.5) + 'px';
-                if (currentPos < -el.offsetWidth) {
+                if (currentPos < -el.offsetWidth - margin) { // Verifica com a margem adicional
                     directionsRight[index] = true; 
                 }
             }
@@ -30,12 +33,12 @@ window.onload = function() {
             const currentPos = parseFloat(getComputedStyle(el).right);
             if (directionsLeft[index]) {
                 el.style.right = (currentPos + 0.5) + 'px';
-                if (currentPos > window.innerWidth) {
+                if (currentPos > window.innerWidth + margin) { // Verifica com a margem adicional
                     directionsLeft[index] = false; 
                 }
             } else {
                 el.style.right = (currentPos - 0.5) + 'px';
-                if (currentPos < -el.offsetWidth) {
+                if (currentPos < -el.offsetWidth - margin) { // Verifica com a margem adicional
                     directionsLeft[index] = true; 
                 }
             }
