1cdf94d2d71225fbc725829ab1d58dba9700ef5d
Laura Pais <pais.laura@gmail.com>
2024-10-17 17:03:19 +0100

    +

--- js/interationAbout.js
+++ js/interationAbout.js
@@ -2,9 +2,15 @@ const moreInfoButton = document.getElementById("moreInfoButton");
 const moreInfoContent = document.getElementById("moreInfoContent");
 
 moreInfoButton.addEventListener("click", () => {
-    if (moreInfoContent.style.display === "block") {
-        moreInfoContent.style.display = "none";
+    if (moreInfoContent.style.maxHeight) {
+        // Se a altura está definida, vamos fechar a div
+        moreInfoContent.style.maxHeight = null;
     } else {
-        moreInfoContent.style.display = "block";
+        // Se não está definida, vamos abrir a div para a altura total do conteúdo
+        moreInfoContent.style.maxHeight = moreInfoContent.scrollHeight + "px";
     }
 });
+
+// Configuração inicial para transição suave no CSS
+moreInfoContent.style.transition = "max-height 0.5s ease";
+moreInfoContent.style.overflow = "hidden";
